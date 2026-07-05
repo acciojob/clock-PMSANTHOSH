bc465e2415acd1e3b0386cc9794955ffb4b74a4b
PMSANTHOSH <145707084+PMSANTHOSH@users.noreply.github.com>
2026-07-05 22:50:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const timer = document.getElementById("timer");
+
+function updateTimer() {
+    const now = new Date();
+    timer.textContent = now.toLocaleString();
+}
+
+// Display immediately
+updateTimer();
+
+// Update every second
+setInterval(updateTimer, 1000);
